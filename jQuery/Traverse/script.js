6e5f7eb028b964de5a056c6b96eb338295c30e08
BIT Student <classroom@bgit.rs>
2019-03-12 18:05:03 +0100

    completed

--- jQuery/Traverse/script.js
+++ jQuery/Traverse/script.js
@@ -7,11 +7,15 @@ $("body").append("<div class = firstDiv></div>");
 $.each(images, function (i, element) {
     $(".firstDiv").append('<img src=' + element + ' />');
     $("img").css("width", "33%");
-    $("img:first").addClass("border");
+    $("img:first").addClass("selected");
 })
 
 $("body").append("<div class = secDiv></div>");
 $.each(images, function (i, element) {
     $(".secDiv").append("<img src = " + element + "/>");
     $("img").css("width", "33%");
-})
+});
+$(function () {
+    $(".firstDiv").children().removeClass("selected")
+    $(".secDiv").children("img:first").next().addClass("selected")
+});
