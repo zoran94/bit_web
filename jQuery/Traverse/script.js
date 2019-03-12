b369928f8b676e7877b54f5c07e813e9f35b69bd
BIT Student <classroom@bgit.rs>
2019-03-12 17:38:03 +0100

    not completed

---
+++ jQuery/Traverse/script.js
@@ -0,0 +1,17 @@
+
+
+var images = ["https://dumielauxepices.net/sites/default/files/medicine-clipart-alternative-medicine-679661-6930158.jpg", "https://dumielauxepices.net/sites/default/files/medicine-clipart-alternative-medicine-679661-6930158.jpg", "https://dumielauxepices.net/sites/default/files/medicine-clipart-alternative-medicine-679661-6930158.jpg"];
+$("body").append("<div class = firstDiv></div>");
+
+
+$.each(images, function (i, element) {
+    $(".firstDiv").append('<img src=' + element + ' />');
+    $("img").css("width", "33%");
+    $("img:first").addClass("border");
+})
+
+$("body").append("<div class = secDiv></div>");
+$.each(images, function (i, element) {
+    $(".secDiv").append("<img src = " + element + "/>");
+    $("img").css("width", "33%");
+})
